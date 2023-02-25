fa6d1f32e3c719b352ff91b4b28cd4c61b0f0dca
George Moschovitis <gmosx@reizu.com>
2023-02-25 22:53:07 +0200

    Demo buttons

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+import { ButtonElement } from "@naisu/naisu-web-components/src/buttons/button.js";
 import { SwitchElement } from "@naisu/naisu-web-components/src/switch/switch.js";
 
 customElements.define("x-switch", SwitchElement);
+customElements.define("x-button", ButtonElement);
