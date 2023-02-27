250549a709c21ef3ed0a1d8c698cf4a7c144cfed
George Moschovitis <gmosx@reizu.com>
2023-02-27 12:11:44 +0200

    Minor

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 import { ButtonElement } from "@naisu/naisu-web-components/src/buttons/button.js";
+import { CircularProgressIndicatorElement } from "@naisu/naisu-web-components/src/indicators/circular-progress-indicator.js";
 import { SwitchElement } from "@naisu/naisu-web-components/src/switch/switch.js";
 
 customElements.define("x-switch", SwitchElement);
 customElements.define("x-button", ButtonElement);
+customElements.define("x-progress-circle", CircularProgressIndicatorElement);
