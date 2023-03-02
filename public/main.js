bdb9604c8acb7a730f7e408fc67e3f0b2807e85b
George Moschovitis <gmosx@reizu.com>
2023-03-02 21:58:14 +0200

    Setup deno import map

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 import { ButtonElement } from "@naisu/naisu-web-components/src/buttons/button.js";
 import { CircularProgressIndicatorElement } from "@naisu/naisu-web-components/src/indicators/circular-progress-indicator.js";
 import { SwitchElement } from "@naisu/naisu-web-components/src/switch/switch.js";
+import { TabElement } from "@naisu/naisu-web-components/src/tabs/tab.js";
 
 customElements.define("x-switch", SwitchElement);
 customElements.define("x-button", ButtonElement);
 customElements.define("x-progress-circle", CircularProgressIndicatorElement);
+customElements.define("x-tab", TabElement);
