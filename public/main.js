c78c159dcbb1dcb8c9a78e99e657894d3574ea79
George Moschovitis <gmosx@reizu.com>
2023-03-04 10:30:27 +0200

    Add typography page

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 import { ButtonElement } from "@naisu/naisu-web-components/buttons/button.js";
+import { DividerElement } from "@naisu/naisu-web-components/divider/divider.js";
 import { CircularProgressIndicatorElement } from "@naisu/naisu-web-components/indicators/circular-progress-indicator.js";
 import { SwitchElement } from "@naisu/naisu-web-components/switch/switch.js";
 import { TabGroupElement } from "@naisu/naisu-web-components/tabs/tab-group.js";
@@ -9,3 +10,4 @@ customElements.define("x-button", ButtonElement);
 customElements.define("x-progress-circle", CircularProgressIndicatorElement);
 customElements.define("x-tab", TabElement);
 customElements.define("x-tab-group", TabGroupElement);
+customElements.define("x-divider", DividerElement);
