8a221a1d812adcea4a702267b7e851f06d8e372a
George Moschovitis <gmosx@reizu.com>
2023-03-04 18:51:06 +0200

    Add tab-group

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ import { CircularProgressIndicatorElement } from "@naisu/naisu-web-components/in
 import { SwitchElement } from "@naisu/naisu-web-components/switch/switch.js";
 import { TabGroupElement } from "@naisu/naisu-web-components/tabs/tab-group.js";
 import { TabElement } from "@naisu/naisu-web-components/tabs/tab.js";
+import { nonNull } from "@reizu/glue/assert.js";
 
 customElements.define("x-switch", SwitchElement);
 customElements.define("x-button", ButtonElement);
@@ -11,3 +12,9 @@ customElements.define("x-progress-circle", CircularProgressIndicatorElement);
 customElements.define("x-tab", TabElement);
 customElements.define("x-tab-group", TabGroupElement);
 customElements.define("x-divider", DividerElement);
+
+const tabs = nonNull(document.querySelector("#tabs"));
+tabs.addEventListener("change", (e) => {
+    const ev = /** @type { CustomEvent } */ (e);
+    console.log("---", ev.detail.active);
+});
